b211a1ed5c136095419b2fa5aaee66f816aa255f
dattu095 <dattu7334@gmail.com>
2026-05-27 17:33:20 +0530

    refact: no planner abstraction needed

--- survey_system/planners/base_planner.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from geometry_msgs.msg import Polygon
-
-
-class BasePlanner(ABC):
-    @abstractmethod
-    def generate_waypoints(self, geofence: Polygon) -> Polygon:
-        pass
